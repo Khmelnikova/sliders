5897c318bb242205e5a9b42adad236e851fca40c
Nadezhda Khmelnikova <nkhmelnikova@gmail.com>
2017-06-28 18:02:50 +0300

    jsons

--- js/main.js
+++ js/main.js
@@ -3,8 +3,52 @@ function start(){
     var xhr = new XMLHttpRequest();
     
     var sliders = [
-        
+        {
+            url: "./src/1.json",
+            pictures: [],
+            display: function(pictures) {}
+        },
+        {
+            url: "./src/1.json",
+            pictures: [],
+            display: function(pictures) {}
+        },
+        {
+            url: "./src/1.json",
+            pictures: [],
+            display: function(pictures) {}
+        },
+        {
+            url: "./src/1.json",
+            pictures: [],
+            display: function(pictures) {}
+        },
+        {
+            url: "./src/1.json",
+            pictures: [],
+            display: function(pictures) {}
+        },
+        {
+            url: "./src/1.json",
+            pictures: [],
+            display: function(pictures) {}
+        }
     ]
+    
+    for(var i in sliders){
+        xhr.open("GET", sliders[i].url, true);
+        xhr.onload = function() {
+            openPage();
+        }
+        xhr.send();
+    }
+    var openPage = function(){
+        for( var j in sliders){
+            sliders[j].pictures = JSON.parse(xhr.responseText);
+            console.log(sliders[j].pictures)
+        }
+    }
+    
 }
 
 start();
