121b6c9734571e8e08029afeb7f8d3263cdd6cd2
Nadezhda Khmelnikova <nkhmelnikova@gmail.com>
2017-07-05 15:10:54 +0300

    2 slider

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ function start(){
             display: function(pictures) {
                 
                 var slides = document.querySelectorAll('#slides .slide');
-                //console.log(pictures, slides)
                 for( var i = 0; i < slides.length; i++){
                     
                     slides[i].innerHTML = '<img src="' + pictures[i].img + '"/>';
@@ -26,7 +25,82 @@ function start(){
         {
             url: "./src/2.json",
             pictures: [],
-            display: function(pictures) {}
+            display: function(pictures) {
+                var slideNow = 1;
+                var slideCount = $('#slidewrapper').children().length;
+                var slideInterval = 3000;
+                var navBtnId = 0;
+                var translateWidth = 0;
+
+                $(document).ready(function() {
+                    var switchInterval = setInterval(nextSlide, slideInterval);
+
+                    $('#viewport').hover(function() {
+                        clearInterval(switchInterval);
+                    }, function() {
+                        switchInterval = setInterval(nextSlide, slideInterval);
+                    });
+
+                    $('#next-btn').click(function() {
+                        nextSlide();
+                    });
+
+                    $('#prev-btn').click(function() {
+                        prevSlide();
+                    });
+
+                    $('.slide-nav-btn').click(function() {
+                        navBtnId = $(this).index();
+
+                        if (navBtnId + 1 != slideNow) {
+                            translateWidth = -$('#viewport').width() * (navBtnId);
+                            $('#slidewrapper').css({
+                                'transform': 'translate(' + translateWidth + 'px, 0)',
+                                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                            });
+                            slideNow = navBtnId + 1;
+                        }
+                    });
+                });
+
+
+                function nextSlide() {
+                    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+                        $('#slidewrapper').css('transform', 'translate(0, 0)');
+                        slideNow = 1;
+                    } else {
+                        translateWidth = -$('#viewport').width() * (slideNow);
+                        $('#slidewrapper').css({
+                            'transform': 'translate(' + translateWidth + 'px, 0)',
+                            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                        });
+                        slideNow++;
+                    }
+                }
+
+                function prevSlide() {
+                    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+                        translateWidth = -$('#viewport').width() * (slideCount - 1);
+                        $('#slidewrapper').css({
+                            'transform': 'translate(' + translateWidth + 'px, 0)',
+                            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                        });
+                        slideNow = slideCount;
+                    } else {
+                        translateWidth = -$('#viewport').width() * (slideNow - 2);
+                        $('#slidewrapper').css({
+                            'transform': 'translate(' + translateWidth + 'px, 0)',
+                            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                        });
+                        slideNow--;
+                    }
+                }
+                
+            }
         },
         {
             url: "./src/3.json",
@@ -65,7 +139,6 @@ function start(){
         
             sliders[n].pictures = JSON.parse(text);
             sliders[n].display(sliders[n].pictures)
-            console.log(sliders[n].pictures)
 
     }
 }
