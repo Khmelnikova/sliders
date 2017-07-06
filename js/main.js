244503e21ab05e7eaebe1b5a8c66d5317d456574
Nadezhda Khmelnikova <nkhmelnikova@gmail.com>
2017-07-06 18:09:44 +0300

    babka

--- js/main.js
+++ js/main.js
@@ -95,8 +95,8 @@ function start(){
                     slider.appendChild(img);
                     img.setAttribute("src", pictures[i].img)
                 }
-                $(document).ready(function() {
-                    $('.slider').tilesSlider({ auto: true })
+                $(function() {
+                    $('.slider').tilesSlider({ effect: 'switchlr', auto: true, loop: true })
                 })
             }
         },
